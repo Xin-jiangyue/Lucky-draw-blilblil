a2339ad22da498fc2ed40abb440704c280fe3814
xinjiangyue <xinjiangyue.cn@gmail.com>
2026-05-08 12:55:01 +0800

    更新 main.py 和 README.md

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 import random as rnd
-blist = pd.read_excel("./抽奖名单.xlsx")
+
+from pip._internal.utils import filetypes
+
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(
+    title="选择名单"
+)
+print(file_path)
+blist = pd.read_excel(file_path)
 MaxNum=len(blist)
-print(MaxNum)
+print("参与抽奖的人数：",MaxNum)
 print(blist)
 rndNum=rnd.randint(1,MaxNum)
-LUCYMAN=blist.iloc[rndNum,2]
-print("获奖者是：",LUCYMAN)
+LP=blist.iloc[rndNum,2]
+print("获奖者是：",LP)
 
