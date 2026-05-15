861aed3c0fd683dfed1fd20b8447fee9949d3bb5
Xin-jiangyue <xinjiangyue.cn@gmail.com>
2026-05-15 18:06:43 +0800

    增加了确认名单的步骤

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
@@ -17,7 +18,15 @@ blist = pd.read_excel(file_path)
 MaxNum=len(blist)
 print("参与抽奖的人数：",MaxNum)
 print(blist)
+while True:
+    cmd = input("\n名单已输出，请按 Y 键继续抽奖,N结束抽奖: ").strip().upper()
+    if cmd == 'Y':
+        break
+    if cmd == 'N':
+        sys.exit()
+    else:
+        print("输入无效，请输入 Y 继续抽奖,N结束抽奖。")
 rndNum=rnd.randint(1,MaxNum)
-LP=blist.iloc[rndNum,2]
+LP=blist.iloc[rndNum,1]
 print("获奖者是：",LP)
 
